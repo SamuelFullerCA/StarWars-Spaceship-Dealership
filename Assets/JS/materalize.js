0006b394aaa63fe4965339a1b3f1c473953d7d8a
SamuelFullerCA <samuelfullerca@gmail.com>
2024-03-02 22:33:58 -0800

    added materialize components and linked swapi with the ability to generate 1 card

---
+++ Assets/JS/materalize.js
@@ -0,0 +1,49 @@
+
+//This function allows the search field to offer autocompleated options
+document.addEventListener('DOMContentLoaded', function() {
+    var inputField = document.querySelector('.autocomplete');
+    M.Autocomplete.init(inputField, {
+        data:{
+            "Death Star": null,
+            "X-Wing": null,
+            "Star Destroyer": null,
+            "A-wing": null,
+            "AA-9 Coruscant freighter": null,
+            "B-wing": null,
+            "Banking clan frigate": null,
+            "CR90 corvette": null,
+            "Calamari Cruiser": null,
+            "Droid control ship": null,
+            "EF76 Nebulon-B escort frigate": null,
+            "Executor": null,
+            "H-type Nubian yacht": null,
+            "Imperial shuttle": null,
+            "J-type diplomatic barge": null,
+            "Jedi Interceptor": null,
+            "Jedi starfighter": null,
+            "Millennium Falcon": null,
+            "Naboo Royal Starship": null,
+            "Naboo fighter": null,
+            "Naboo star skiff": null,
+            "Rebel transport": null,
+            "Republic Assault ship": null,
+            "Republic Cruiser": null,
+            "Republic attack cruiser": null,
+            "Scimitar": null,
+            "Sentinel-class landing craft": null,
+            "Slave 1": null,
+            "Solar Sailer": null,
+            "TIE Advanced x1": null,
+            "Theta-class T-2c shuttle": null,
+            "Trade Federation cruiser": null,
+            "V-wing": null,
+            "Y-wing": null,
+            "arc-170": null
+        },
+
+        //current limit of how many displayed at a given time is 5
+        limit:5
+    })
+  });
+
+
--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,25 +1,56 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Stack from '@mui/material/Stack';
-import Slider from '@mui/material/Slider';
-import VolumeDown from '@mui/icons-material/VolumeDown';
-import VolumeUp from '@mui/icons-material/VolumeUp';
 
-export default function ContinuousSlider() {
-  const [value, setValue] = React.useState(30);
+// catchall variable of data that is not locked to a function
+let searchedData;
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
 
-  return (
-    <Box sx={{ width: 200 }}>
-      <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
-        <VolumeDown />
-        <Slider aria-label="Volume" value={value} onChange={handleChange} />
-        <VolumeUp />
-      </Stack>
-      <Slider disabled defaultValue={30} aria-label="Disabled slider" />
-    </Box>
-  );
+//Function that pulls an array of starships based off of user input
+async function searhApi(){
+  formValue = document.querySelector('#autocomplete-input').value
+  userSearch = `https://swapi.dev/api/starships/?search=${formValue}`
+
+  //fetches the data and stores it to the cathall variable
+  const response = await fetch(userSearch)
+  let fetchedData = await response.json()
+  console.log(fetchedData)
+  searchedData = fetchedData
+
+  
+  //deletes the cards generated from previous search
+  const previousCards = document.querySelector('.starship')
+  if (previousCards !== null){
+    previousCards.remove()
+  }
+
+  //runs the function to create a card
+  starshipCards()
 }
+
+//function to generate card
+function starshipCards(){
+
+let starshipCard = document.createElement('section')
+//col s3 class allows it to accupy 3/12 of the row, so 4 cards per row
+starshipCard.setAttribute('class', 'starship ship1 col s3')
+
+let starshipName = document.createElement('h3')
+starshipName.textContent = `${searchedData.results[0].name}`
+
+let starshipModel = document.createElement('p')
+starshipModel.textContent = `${searchedData.results[0].model}`
+
+document.querySelector('#displayedCards').append(starshipCard)
+document.querySelector('.ship1').append(starshipName, starshipModel)
+
+
+// shaceship variables we can use include:
+// cargo_capacity cost_in_credits crew hyperdrive_rating manufacturer name starship_class model max_atmosphering_speed passengers consumables
+
+
+
+
+
+}
+
+
+const search = document.querySelector("#searchBtn")
+search.addEventListener('click', searhApi);
