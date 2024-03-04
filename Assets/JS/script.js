3a16428699f2ed452ca3d85962bece338cfdf29a
SamuelFullerCA <samuelfullerca@gmail.com>
2024-03-04 10:05:10 -0800

    implemented google image api into project

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,18 +1,30 @@
 
 // catchall variable of data that is not locked to a function
 let searchedData;
+let imageData;
 
 
 //Function that pulls an array of starships based off of user input
 async function searhApi(){
+
   formValue = document.querySelector('#autocomplete-input').value
+  //quick test for google image api
+  test2 = `https://www.googleapis.com/customsearch/v1?key=AIzaSyD3Lznxmq6WeZS28GXXXD3JQE5_m1PCatU&cx=21a0a191a509143c4&searchType=image&q=${formValue}`
+
+  const response = await fetch(test2)
+  let fetchedData = await response.json()
+  console.log(fetchedData)
+  imageData = fetchedData
+
+
+
   userSearch = `https://swapi.dev/api/starships/?search=${formValue}`
 
   //fetches the data and stores it to the cathall variable
-  const response = await fetch(userSearch)
-  let fetchedData = await response.json()
-  console.log(fetchedData)
-  searchedData = fetchedData
+  const response2 = await fetch(userSearch)
+  let fetchedData2 = await response2.json()
+  console.log(fetchedData2)
+  searchedData = fetchedData2
 
   
   //deletes the cards generated from previous search
@@ -23,7 +35,7 @@ async function searhApi(){
 
   //runs the function to create a card
   starshipCards()
-}
+
 
 //function to generate card
 function starshipCards(){
@@ -32,14 +44,25 @@ let starshipCard = document.createElement('section')
 //col s3 class allows it to accupy 3/12 of the row, so 4 cards per row
 starshipCard.setAttribute('class', 'starship ship1 col s3')
 
+let starshipImg = document.createElement('IMG')
+starshipImg.setAttribute('src', `${imageData.items[0].link}`)
+starshipImg.setAttribute('width', '300')
+starshipImg.setAttribute('height', '300')
+starshipImg.setAttribute('class', 'imgCss')
+
+
 let starshipName = document.createElement('h3')
 starshipName.textContent = `${searchedData.results[0].name}`
 
 let starshipModel = document.createElement('p')
 starshipModel.textContent = `${searchedData.results[0].model}`
 
+let addCart = document.createElement('button')
+addCart.setAttribute('id', `${searchedData.results[0].name}`)
+
+
 document.querySelector('#displayedCards').append(starshipCard)
-document.querySelector('.ship1').append(starshipName, starshipModel)
+document.querySelector('.ship1').append(starshipName, starshipModel, starshipImg, addCart)
 
 
 // shaceship variables we can use include:
@@ -50,7 +73,23 @@ document.querySelector('.ship1').append(starshipName, starshipModel)
 
 
 }
+}
 
 
 const search = document.querySelector("#searchBtn")
 search.addEventListener('click', searhApi);
+
+
+// async function googleimageApi(){
+
+//   test = 'https://cse.google.com/cse.js?cx=92c28db4b1c7148d3'
+//   test2 = 'https://www.googleapis.com/customsearch/v1?key=AIzaSyD3Lznxmq6WeZS28GXXXD3JQE5_m1PCatU&cx=21a0a191a509143c4&searchType=image&q=deathstar'
+
+//   const response = await fetch(test2)
+//   let fetchedData = await response.json()
+//   imageData = fetchedData
+
+//   console.log(fetchedData)
+// }
+
+
