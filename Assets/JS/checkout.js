33cb44098b938c0333d406f2bf9b157da2adb58f
Willie Yeh <willieyeh65@gmail.com>
2024-03-07 17:34:55 -0800

    Made modal pop up on checkout form

--- Assets/JS/checkout.js
+++ Assets/JS/checkout.js
@@ -1,23 +1,39 @@
-function checkout() {
-const card = document.createElement('div');
-card.setAttribute('class','cards')
-document.getElementID('checkout').appendChild(card)
+// function checkout() {
+// const card = document.createElement('div');
+// card.setAttribute('class','cards')
+// document.getElementID('checkout').appendChild(card)
 
-// Create h2 element for title
-const title = document.createElement('h2');
-title.textContent = 'Title';
-
-
-// Create p element for description
-const description = document.createElement('p');
-description.textContent = 'Description';
+// // Create h2 element for title
+// const title = document.createElement('h2');
+// title.textContent = 'Title';
 
 
-// Append title and description to the card
+// // Create p element for description
+// const description = document.createElement('p');
+// description.textContent = 'Description';
 
 
-// Append the card to the body or any other container
-document.querySelector('.cards').appendChild(title, description);
+// // Append title and description to the card
 
-checkout()
+
+// // Append the card to the body or any other container
+// document.querySelector('.cards').appendChild(title, description);
+
+// checkout()
+function onSubmit (event){
+event.preventDefault()
+if  (isValid()){
+    document.querySelector("#submitButton").setAttribute('class', 'modal-trigger')
+    console.log('Hello')
+    
+}else {
+document.querySelector("#submitButton").removeAttribute('class', 'modal-trigger')
+}
+}
+function isValid (){
+    return(document.querySelector('#name').value !== "" &&
+document.querySelector('#email').value !== "" &&
+document.querySelector('#address').value !== "" &&
+document.querySelector('#city').value !== "" &&
+document.querySelector('#zip').value !== "")
 }
