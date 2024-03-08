0d8b843071766dccbdf52dba368477b8abd42bd1
Willie Yeh <willieyeh65@gmail.com>
2024-03-08 13:36:36 -0800

    Modal design fixed with content

--- Assets/JS/checkout.js
+++ Assets/JS/checkout.js
@@ -1,23 +1,60 @@
-function checkout() {
-const card = document.createElement('div');
-card.setAttribute('class','cards')
-document.getElementID('checkout').appendChild(card)
 
-// Create h2 element for title
-const title = document.createElement('h2');
-title.textContent = 'Title';
+let planetGen;
+const planets = ["Exegol", "Bracca", "Nevarro", "Dantooine", "Chandrila", "Hoth", "Morak", "Ajan Kloss"];
+//Activates modal to pop up
+function onSubmit (event){
+    event.preventDefault()
+        modalInstance.open();
+    }
+    
+    let modalElement = document.querySelectorAll('#modal1');
+    let modalInstance;
+    
+    document.addEventListener('DOMContentLoaded', function() {
+        console.log('hello')
+        modalInstance = M.Modal.init(modalElement,)[0];
+      });
 
 
-// Create p element for description
-const description = document.createElement('p');
-description.textContent = 'Description';
+const randomPlanet = Math.floor(Math.random() * planets.length)
+   
 
 
-// Append title and description to the card
+
+const modalDetails = document.querySelector("#submitButton")
+modalDetails.addEventListener('click', modalContent)
 
 
-// Append the card to the body or any other container
-document.querySelector('.cards').appendChild(title, description);
 
-checkout()
+//Makes modal content
+function modalContent(){
+
+    let nuke = document.querySelector('#nukeme')
+    if(nuke !== null){
+        nuke.remove()
+    }
+
+
+    let section = document.createElement('section')
+    section.setAttribute('class','card grey darken-4')
+    section.setAttribute('id', 'nukeme')
+    document.querySelector('#modal-Content').append(section)
+
+    let header = document.createElement('h1')
+    header.style.color = 'white'
+    header.textContent = "Pickup"
+
+    let paragraph = document.createElement('p')
+    paragraph.style.fontSize = '150%'
+    paragraph.style.color = 'white'
+    paragraph.textContent = `Please pickup at ${planets[randomPlanet]}. Please notify us if you cannot pickup vehicle within 2 weeks of pickup time`
+
+    document.querySelector('.card').append(header, paragraph)
+    
+    // if(paragraph !== null){
+    //     paragraph.remove()
+    // }
+
+  
 }
+
